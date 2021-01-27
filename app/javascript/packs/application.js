05c6925105320aba1dd1713efa1d2f1df5e1ce87
Thomas <th.farre@gmail.com>
2021-01-27 16:28:52 +0100

    config tailwind ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -12,6 +14,8 @@ require("channels")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
+
+import '../stylesheets/application.scss';
+
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
