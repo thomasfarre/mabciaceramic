116b424a1a0ece2c05a9f3b642e194825cd21993
Thomas <th.farre@gmail.com>
2021-01-28 14:20:57 +0100

    add item index/show view and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         registrations: 'registrations'
       }
+
   root to: 'pages#home'
+
+  resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
