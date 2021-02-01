65e40dbadbc3b0341a0ddc641f2c91e986a6cfb4
Thomas <th.farre@gmail.com>
2021-02-01 10:51:41 +0100

    added cart helper methods and view to see if user has a cart link to it and see how many items are on it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def cart_count_over_one
+    if @cart.cart_items.count > 0
+      return "#{@cart.cart_items.count}"
+    end
+  end
+
+  def cart_has_items
+    return @cart.cart_items.count > 0
+  end
 end
