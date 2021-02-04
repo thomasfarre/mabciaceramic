9cf4b44054743781f71d6bcfce9d662ca88c2571
Thomas <th.farre@gmail.com>
2021-02-04 12:06:20 +0100

    flow to buy ok until you pay

--- app/controllers/adresses_controller.rb
+++ app/controllers/adresses_controller.rb
@@ -4,22 +4,30 @@ class AdressesController < ApplicationController
     if current_user.adresses.empty?
       @adress = Adress.new
     else
-      @adress = Adress.find(params[:id])
+      @adress = current_user.adresses.first
     end
+  end
 
-
+  def create
+    @adress = Adress.new(adress_params)
+    @adress.user = current_user
+    if @adress.save
+      redirect_to new_order_payment_path(@cart.order)
+    else
+      render :edit
+    end
   end
 
   def update
     @adress = Adress.find(params[:id])
     if @adress.update(adress_params)
-      redirect_to root_path
+      redirect_to new_order_payment_path(@cart.order)
     else
       render :edit
     end
   end
 
   def adress_params
-    params.require(:adress).permit(:street, :street_detail, :zipcode, :country, :phone)
+    params.require(:adress).permit(:street, :street_detail, :zipcode, :country, :phone, :city)
   end
 end
