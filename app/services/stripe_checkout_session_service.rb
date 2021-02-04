fe547476b97dc59770bac6f542f0611804682a16
Thomas <th.farre@gmail.com>
2021-02-04 17:44:22 +0100

    changing items status not working for now

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,6 +2,10 @@ class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
+
+    # This doesn't work yet don't know why
+    # order.cart.cart_items { |item| item.item.update(status: 'removed') }
+
     order.cart.destroy
   end
 end
