9c928d1dc15f88e1b737931bcbe97ac1b66beed4
Thomas <th.farre@gmail.com>
2021-02-06 16:54:22 +0100

    add x to close menu on navbar

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,10 +1,12 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-   static targets = [ "toggleable" ]
+   static targets = [ "toggleable", "open", "close" ]
 
 
    toggle() {
      this.toggleableTarget.classList.toggle('hidden')
+     this.openTarget.classList.toggle('hidden')
+     this.closeTarget.classList.toggle('hidden')
    }
 }
