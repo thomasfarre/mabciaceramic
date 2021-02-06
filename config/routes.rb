7a7edce8f89717ad3c74391428d7ce926d465166
Thomas <th.farre@gmail.com>
2021-02-06 20:40:35 +0100

    add about route + view and content

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,10 @@ Rails.application.routes.draw do
     resources :adresses
   end
 
+
   root to: 'pages#home'
+  get '/about/', to: 'pages#about'
+
   mount StripeEvent::Engine, at: '/stripe-webhooks' # provide a custom path
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
