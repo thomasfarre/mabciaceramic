3b30616e01565a4a0ad10ba3d223e867f48e3140
Thomas <th.farre@gmail.com>
2021-02-07 12:32:19 +0100

    fix to sticky logo

--- app/javascript/controllers/stickylogo_controller.js
+++ app/javascript/controllers/stickylogo_controller.js
@@ -6,12 +6,10 @@ export default class extends Controller {
 
    onScroll() {
     var y = window.scrollY;
-    if (y >= 50) {
-     this.stickylogoTarget.classList.remove('opacity-0')
-     this.stickylogoTarget.classList.add('opacity-100')
+    if (y >= 60) {
+     this.stickylogoTarget.classList.remove('hidden')
     } else {
-        this.stickylogoTarget.classList.remove('opacity-100')
-        this.stickylogoTarget.classList.add('opacity-0')
+        this.stickylogoTarget.classList.add('hidden')
     }
    };
 }
