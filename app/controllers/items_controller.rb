ae6e30545de9a38674b367faf95e716e3cd8b4d0
Thomas <th.farre@gmail.com>
2021-02-23 17:55:47 +0100

    fix link to category from landing page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,8 +11,13 @@ class ItemsController < ApplicationController
       sql_query = "title ILIKE :query OR category ILIKE :query"
       @items = Item.where(sql_query, query: "%#{params[:query]}%")
     else
+      if(params.key?(:category))
+        @items = Item.where(category: params[:category]).order("created_at desc")
+      else
+        @items = Item.all.order("created_at desc")
+      end
       @page = params.fetch(:page, 0).to_i
-      @items = Item.offset(@page * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+      @items = @items.offset(@page * ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
     end
   end
 
