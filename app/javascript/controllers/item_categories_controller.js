d8c90bf15f8b881867db4fa099c1fee2334e016b
Thomas <th.farre@gmail.com>
2021-02-24 11:05:17 +0100

    pagination in vanilia js working for everyitems

--- app/javascript/controllers/item_categories_controller.js
+++ app/javascript/controllers/item_categories_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
 
-   static targets = [ "category", "button", "toutVoir", "toutVoirButton" ]
+   static targets = [ "category", "button", "toutVoir", "toutVoirButton", "items", "toto" ]
 
 
 
@@ -43,4 +43,104 @@ export default class extends Controller {
       category.classList.add('hidden')
     })
   }
+
+  connect() {
+
+
+    var state = {
+      'querySet': this.itemsTargets,
+      'page': 1,
+      'rows': 1,
+      'window': 5,
+    }
+
+    var pagesContainer = this.totoTarget
+    // var table = this.toutVoirTarget
+
+    buildTable()
+
+    function pagination(querySet, page, rows) {
+
+      var trimStart = (page - 1) * rows
+      var trimEnd = trimStart + rows
+
+      var trimmedData = querySet.slice(trimStart, trimEnd)
+
+      var pages = Math.round(querySet.length / rows);
+
+      return {
+          'querySet': trimmedData,
+          'pages': pages,
+      }
+    }
+
+
+    function pageButtons(pages) {
+
+        console.log(pagesContainer)
+
+        var wrapper = pagesContainer
+
+        wrapper.innerHTML = ``
+      console.log('Pages:', pages)
+
+        var maxLeft = (state.page - Math.floor(state.window / 2))
+        var maxRight = (state.page + Math.floor(state.window / 2))
+
+        if (maxLeft < 1) {
+            maxLeft = 1
+            maxRight = state.window
+        }
+
+        if (maxRight > pages) {
+            maxLeft = pages - (state.window - 1)
+
+            if (maxLeft < 1){
+              maxLeft = 1
+            }
+            maxRight = pages
+        }
+
+
+
+        for (var page = maxLeft; page <= maxRight; page++) {
+          wrapper.innerHTML += `<button value=${page} class="page px-2 py-1 bg-japonica-500 text-white font-bold">${page}</button>`
+        }
+
+        if (state.page != 1) {
+            wrapper.innerHTML = `<button value=${1} class="page px-2 py-1 bg-japonica-500 text-white font-bold"> First </button>` + wrapper.innerHTML
+        }
+
+        if (state.page != pages) {
+            wrapper.innerHTML += `<button value=${pages} class="page px-2 py-1 bg-japonica-500 text-white font-bold">Last </button>`
+        }
+
+        $('.page').on('click', function() {
+            $('#table').empty()
+
+            state.page = Number($(this).val())
+
+            buildTable()
+        })
+
+    }
+
+    function buildTable() {
+
+        var table = $('#table')
+
+        var data = pagination(state.querySet, state.page, state.rows)
+
+        var myList = data.querySet
+
+        for (var i in myList) {
+
+                table.append(myList)
+            }
+
+        pageButtons(data.pages)
+    }
+
+  }
+
 }
