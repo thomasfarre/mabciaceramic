f1e17623b0702c5be61bf930e0dea64ade691dd8
Thomas <th.farre@gmail.com>
2021-03-02 10:18:44 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method     = :postmark
-  config.action_mailer.postmark_settings   = { api_token: ENV['POSTMARK_API_TOKEN'] }
-  config.action_mailer.default_url_options = { host: "www.mabciaceramic.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp-relay.sendinblue.com",
+    port: 587,
+    user_name: ENV['SENDINBLUE_EMAIL'],
+    password: ENV['SENDINBLUE_PASSWORD'],
+    authentication: login,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
