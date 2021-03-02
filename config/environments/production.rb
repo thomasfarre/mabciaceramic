aa9d868857e5275734f2872d6b77efc4fd868301
Thomas <th.farre@gmail.com>
2021-03-02 10:49:02 +0100

    waiting for sendinblue activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,16 +62,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp-relay.sendinblue.com",
-    port: 587,
-    user_name: ENV['SENDINBLUE_EMAIL'],
-    password: ENV['SENDINBLUE_PASSWORD'],
-    authentication: 'login',
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp-relay.sendinblue.com",
+  #   port: 587,
+  #   user_name: ENV['SENDINBLUE_EMAIL'],
+  #   password: ENV['SENDINBLUE_PASSWORD'],
+  #   authentication: 'login',
+  #   enable_starttls_auto: true
+  # }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
