10cbc3e4813a5a7920d892a63a55a1f4603409ca
Thomas <th.farre@gmail.com>
2021-03-02 14:59:23 +0100

    front change to menu and buttons on navbar

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,14 @@ class Order < ApplicationRecord
 
   belongs_to :user
   monetize :amount_cents
+
+  validates :status, inclusion: { in: ['paid', 'checked', 'envoyé'] }
+
+  def paid?
+    status == 'paid'
+  end
+
+  def envoyé?
+    status == 'envoyé'
+  end
 end
