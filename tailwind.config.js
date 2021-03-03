f6951d87ae8f560ac30811be5fa3414a34766064
Thomas <th.farre@gmail.com>
2021-03-03 17:07:12 +0100

    cart show ok on all device

--- tailwind.config.js
+++ tailwind.config.js
@@ -141,6 +141,7 @@ module.exports = {
       8: '8px',
     },
     boxShadow: {
+      total: '0 4px 20px rgb(34 34 34 / 15%)',
       sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
       DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
       md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
