09817a1a4e4e56fe246e96857e7af51285c946b8
Thomas <th.farre@gmail.com>
2021-03-08 11:34:05 +0100

    try to fix

--- config/initializers/send_in_blue.rb
+++ config/initializers/send_in_blue.rb
@@ -1,22 +1,3 @@
-# Setup authorization
 SibApiV3Sdk.configure do |config|
-  # Configure API key authorization: api-key
   config.api_key['api-key'] = ENV["SEND_IN_BLUE_API_KEY"]
-  # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
-  #config.api_key_prefix['api-key'] = 'Bearer'
-
-  # Configure API key authorization: partner-key
-  config.api_key['partner-key'] = ENV["SEND_IN_BLUE_API_KEY"]
-  # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
-  #config.api_key_prefix['partner-key'] = 'Bearer'
-end
-
-api_instance = SibApiV3Sdk::AccountApi.new
-
-begin
-  #Get your account information, plan and credits details
-  result = api_instance.get_account
-  p result
-rescue SibApiV3Sdk::ApiError => e
-  puts "Exception when calling AccountApi->get_account: #{e}"
 end
