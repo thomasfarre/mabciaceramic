105c0d1f9127f32d81e095adf6fc662eaf655814
Thomas <th.farre@gmail.com>
2021-03-08 16:15:45 +0100

    refacto stimulus controllers

--- app/javascript/controllers/stickylogo_controller.js
+++ app/javascript/controllers/stickylogo_controller.js
@@ -1,15 +1,15 @@
-import { Controller } from "stimulus";
+// import { Controller } from "stimulus";
 
-export default class extends Controller {
-   static targets = [ "stickylogo" ]
+// export default class extends Controller {
+//    static targets = [ "stickylogo" ]
 
 
-   onScroll() {
-    var y = window.scrollY;
-    if (y >= 60) {
-     this.stickylogoTarget.classList.add('lg:block')
-    } else {
-        this.stickylogoTarget.classList.remove('lg:block')
-    }
-   };
-}
+//    onScroll() {
+//     var y = window.scrollY;
+//     if (y >= 60) {
+//      this.stickylogoTarget.classList.add('lg:block')
+//     } else {
+//         this.stickylogoTarget.classList.remove('lg:block')
+//     }
+//    };
+// }
