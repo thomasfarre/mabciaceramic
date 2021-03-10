59d88dcae7c2300b73faba89cdd4a2dc73ed99eb
Thomas <th.farre@gmail.com>
2021-03-10 11:57:21 +0100

    add privacy page

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/about/', to: 'pages#about'
   get '/terms/', to: 'pages#terms'
+  get '/privacy/', to: 'pages#privacy'
 
   mount StripeEvent::Engine, at: '/stripe-webhooks' # provide a custom path
 
