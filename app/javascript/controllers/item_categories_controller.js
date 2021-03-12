4b067bfb02359367cf677885b2a5a27f9be012c5
Thomas <th.farre@gmail.com>
2021-03-12 22:39:57 +0100

    small changes front

--- app/javascript/controllers/item_categories_controller.js
+++ app/javascript/controllers/item_categories_controller.js
@@ -6,11 +6,11 @@ export default class extends Controller {
 
 // my 3 way of displaying items, first is when i arrived on the page you see all items, second is when you click on a category and last when you click back to all items
   connect() {
-    this.allButtonTarget.classList.remove('text-japonica-400')
+    this.allButtonTarget.classList.remove('text-japonica-500')
     this.allButtonTarget.classList.add('text-white', 'bg-japonica-500')
     this.categoryButtonTargets.forEach(categoryButton => {
       categoryButton.classList.remove('text-white', 'bg-japonica-500')
-      categoryButton.classList.add('text-japonica-400')
+      categoryButton.classList.add('text-japonica-500')
     })
 
     let state = this.setupPagination(this.getAll())
@@ -22,15 +22,15 @@ export default class extends Controller {
   display(event) {
 
     let type = event.target.getAttribute("data-type")
-    event.currentTarget.classList.remove('text-japonica-400')
+    event.currentTarget.classList.remove('text-japonica-500')
     event.currentTarget.classList.add('text-white', 'bg-japonica-500')
 
     this.categoryButtonTargets.filter(categoryButton => {
      if (categoryButton.getAttribute("data-type") != type){
       categoryButton.classList.remove('text-white', 'bg-japonica-500')
-      categoryButton.classList.add('text-japonica-400') }})
+      categoryButton.classList.add('text-japonica-500') }})
     this.allButtonTarget.classList.remove('text-white', 'bg-japonica-500')
-    this.allButtonTarget.classList.add('text-japonica-400')
+    this.allButtonTarget.classList.add('text-japonica-500')
 
 
 
@@ -134,13 +134,13 @@ export default class extends Controller {
           wrapper.innerHTML += `<button value=${page} class="page px-2 py-1 bg-gray-100 text-gray-700 font-bold">${page}</button>`
         }
 
-        if (state.page != 1) {
-            wrapper.innerHTML = `<button value=${1} class="page px-2 py-1 bg-gray-100 text-gray-700 font-bold"> First </button>` + wrapper.innerHTML
-        }
+        // if (state.page != 1) {
+        //     wrapper.innerHTML = `<button value=${1} class="page px-2 py-1 bg-gray-100 text-gray-700 font-bold"> First </button>` + wrapper.innerHTML
+        // }
 
-        if (state.page != pages) {
-            wrapper.innerHTML += `<button value=${pages} class="page px-2 py-1 bg-gray-100 text-gray-700 font-bold">Last </button>`
-        }
+        // if (state.page != pages) {
+        //     wrapper.innerHTML += `<button value=${pages} class="page px-2 py-1 bg-gray-100 text-gray-700 font-bold">Last </button>`
+        // }
 
         $('.page').on('click', function() {
 
