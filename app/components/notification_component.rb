7b0762708686dc70ae421454d72c33609d3cb632
Thomas <th.farre@gmail.com>
2021-03-23 11:02:48 +0100

    try something for notification pb

--- app/components/notification_component.rb
+++ app/components/notification_component.rb
@@ -13,7 +13,7 @@ class NotificationComponent < ViewComponent::Base
     @icon_class = icon_class
     @icon_color_class = icon_color_class
 
-    @data[:timeout] ||= 4
+    @data[:timeout] ||= 60
   end
 
   private
