2d7429146813720264c86ca6c2d9dadac0c6b994
Thomas <th.farre@gmail.com>
2021-03-24 10:34:21 +0100

    few changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :disable_nav, :disable_footer, only: [:show]
+  # before_action :disable_nav, :disable_footer, only: [:show]
 
   def new
     @article = Article.new
